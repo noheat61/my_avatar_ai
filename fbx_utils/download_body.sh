0865ae6aa8934e52e9d8dda8ba2252a4d8dc6fb1
pajamacoder <codespectator@gmail.com>
2022-11-19 14:33:45 +0900

    add hair

--- fbx_utils/download_body.sh
+++ fbx_utils/download_body.sh
@@ -2,6 +2,6 @@ gdown --id 16zSrjnm5CsyiOAkzOXA2H5Xzr49agrXn -O body_sample.zip
 unzip -o body_sample.zip -d fbx_utils/body
 rm body_sample.zip
 
-gdown --id 1VvvCZMClstBNtDWvrbKuHz_RHit-NyU- -O hair_sample.zip
+gdown --id 1SJL2d8jncGh1yQRL5quiD2ciAvP1qGEf -O hair_sample.zip
 unzip -o hair_sample.zip -d fbx_utils/hair
 rm hair_sample.zip
